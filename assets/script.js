3ac187521b8bc958e6558743f553793dc2b9c55b
binhta <binhta102@gmail.com>
2022-03-17 23:35:50 -0400

    first push

---
+++ assets/script.js
@@ -0,0 +1,43 @@
+var questions =[
+    {
+        question: "What is a pseudo-class?",
+        options: ["A CSS rule that contains no declarations.","A CSS declaration that hides the element.","An element that has more than one class.","A CSS keyword to target an element's state."],
+        answer: "A CSS keyword to target an element's state."
+    
+    },
+    {
+        question:"What is an example of a pseudo-element?",
+        options: ["::before","::after","::first-letter","all of the above"],
+        answer: "all of the above"
+
+    },
+    {
+        question:"what does the Z-index property do?",
+        options: ["Removes an element from the DOM","changes the stacking order of elements","Changes the opacity of an element.","Forces an element to be positioned relatively."],
+        answer: "changes the stacking order of elements"
+
+    },
+    {
+        question:"What do media queries allow us to do?",
+        options: ["Play videos on our page"," create responsive designs","change css at different browser widths","do nothing"],
+        answer: "change css at different browser widths"
+
+    },
+    {
+        question:"Which one of these is NOT a valid media type for media queries?",
+        options: ["all","screen","speech","tablet"],
+        answer: "input"
+
+    },
+]
+
+
+
+
+
+
+
+
+
+
+
